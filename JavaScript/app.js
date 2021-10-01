e46f01a925e156beea0049ddc01bf8158419aa93
Brian <twene521@gmail.com>
2021-10-02 00:09:55 +0100

    Things that have been completed:
    Timer is is fully functional:
    The user is able to set a time to countdown from and can reset the timer
    
    The timer will alert the user if its done.
    This is only done by a popup with no sound
    
    Things that are still left to be implemented:
    - Progress bar that's active while the timer is running
    - Sound option settings: user is able to choose from a selection of sounds
    - Dark Mode feature

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,142 @@
 "use strict"
+//filename: app.js
+//Author: Brian Twene
+//Date: 14/09/21
+
+//This file handles the timer operation
+//work is still in progress to finish the progress bar, sound options and dark mode...
+
+//store html elements in variables
 const prefBtn = document.querySelector(".pref_btn");
 const sideMenu = document.querySelector(".menu-background");
 const userInput = document.querySelectorAll(".timer")
 const start = document.querySelector(".start-pause");
 const reset = document.querySelector(".reset");
-const timer = document.querySelector(".timer-display");
+const timerDisplay = document.querySelector(".timer-display");
+const progress = document.querySelector(".progress");
+const progress_contain = document.querySelector(".progress-container")
 
 
 
+
+
+// timer class
+class Timer{
+
+    //instance variables
+    constructor(hrs, min, sec){
+        this.hrs = hrs;
+        this.min = min;
+        this.sec = sec;
+        this.start;
+        this.reset;
+        this.totaltime;
+        this.timeLeft;
+        this.progress;
+    }
+
+    //method for displaying errors
+    errorAlert(){
+        alert("Invalid Input");
+    }
+
+    //countdown method
+    countdown(){
+        userInput[0].value = this.hrs;
+        userInput[1].value = this.min;
+        userInput[2].value = this.sec;
+
+        if(this.hrs === 0 && this.min === 0 && this.sec === 0){
+            this.reset()
+            alert("timer fin");
+        }
+
+        if(this.min === 0){
+            if(this.hrs > 0 && this.sec === 0){
+                this.hrs--;
+                this.min = 60;
+            }
+        }
+
+        if(this.sec === 0){
+            if(this.min > 0){
+                this.min--;
+                this.sec = 60;
+            }
+        }
+
+        if(this.sec > 0){
+            this.sec--;
+        }
+        
+
+    }
+
+    //method for decrementing the units 
+    unitDecrement(unit1, unit2){
+        if(unit1 === 0){
+            if(unit2 > 0){
+                unit2--;
+                unit1 = 60;
+            }
+        }
+    }
+
+    //method for actively displaying the progress - in development
+    progressBar(){
+        this.timeLeft = this.totalTime
+
+        this.timeLeft--
+        progress.style.width = `${(totalLeft/this.totaltime)*100}%`
+    }
+
+    //method for starting the progress bar - in development
+    startProgress(){
+        this.totalTime = this.calcuateTotalSec()
+
+        this.progress = setInterval(this.progressBar.bind(this), 1000)
+
+
+    }
+
+    //function for calculating the total time - in progress
+    calcuateTotalSec(){
+       let totalHrs = this.hrs*60*60;
+       let totalMin = this.min*60;
+
+
+        return this.sec + totalHrs + totalMin;
+    }
+
+    //start countdown method
+    start(){
+        // this.start = window.setInterval(this.countdown.bind(this),1000);
+        this.progress = setInterval(this.startProgress.bind(this), 1000)
+    }
+
+    //reset timer method
+    reset(){
+        this.reset = window.clearInterval(this.start);
+        progress_contain.classList.toggle("progress-active")
+        userInput[0].value = null;
+        userInput[1].value = null;
+        userInput[2].value = null;
+        reset.classList.toggle("hidden")
+        start.classList.toggle("hidden")
+        for(let i = 0; i < userInput.length; i++){
+            userInput[i].removeAttribute("readonly","");
+            userInput[i].classList.toggle("active");
+        }
+    }
+
+
+    
+}
+
+
+//event listener for if the user clicks the start button
 start.addEventListener("click", function(){
+    let timer1 = null;
 
     if( userInput[0].value === "" && userInput[1].value === "" && userInput[2].value === ""){
         alert("invalid input")
@@ -25,72 +153,42 @@ start.addEventListener("click", function(){
         let min = parseInt(userInput[1].value === "" ? 0 : userInput[1].value);
         let sec = parseInt(userInput[2].value === "" ? 0 : userInput[2].value);
 
-        const countdown = function(){
+        timer1 = new Timer(hrs,min,sec);
 
-            userInput[0].value = hrs;
-            userInput[1].value = min;
-            userInput[2].value = sec;
+        progress_contain.classList.toggle("progress-active")
 
-            if(hrs === 0 && min === 0 && sec === 0){
-                clearInterval(going)
-                console.log("timer fin");
-            }
+        timer1.countdown();
+        timer1.start();
 
-            if(min === 0){
-                if(hrs > 0){
-                    hrs--;
-                    min = 60;
-                }
-            }
-            
-            if(sec === 0){
-                if(min > 0){
-                    min--;
-                    sec = 60;
-                }
-            }
 
-            
-            if(sec > 0){
-                sec--;
-            }
-        
     }
-
-    let going = setInterval(countdown,1000);
-
+    
     reset.addEventListener("click", function(){
-        clearInterval(going)
-        userInput[0].value = 0;
-        userInput[1].value = 0;
-        userInput[2].value = 0;
-        reset.classList.toggle("hidden")
-        start.classList.toggle("hidden")
-        for(let i = 0; i < userInput.length; i++){
-            userInput[i].removeAttribute("readonly","");
-            userInput[i].classList.toggle("active");
-        }
+        timer1.reset();
     })
-
-    }
+   
 
 })
 
 
 
+
+//event listener for side menu
 prefBtn.addEventListener("click", function(){
     sideMenu.classList.toggle("open");
     
 
 })
 
+//event listener for closing the side menu
 sideMenu.addEventListener("click", function(e){
     if(e.target === sideMenu){
         sideMenu.classList.toggle("open");
     }
 })
 
-timer.addEventListener("input", function(e){
+//event listener for checking if the user has interacted with the input box
+timerDisplay.addEventListener("input", function(e){
     if(e.target.tagName === "INPUT"){
         console.log(e.target);
         validateInput(e.target);
@@ -98,9 +196,11 @@ timer.addEventListener("input", function(e){
     }
 })
 
+//function for setting a limit.
 const validateInput = function(input){
     if (input.value < 0) input.value = 0;
     if (input.value > 59) input.value = 59;
 }
+
 
 
